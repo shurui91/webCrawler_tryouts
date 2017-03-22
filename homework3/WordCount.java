c05e7e19a3346d3c6afc8039cc5e0eb4a0c847c3
Shurui Liu <shurui91@gmail.com>
2017-03-21 20:22:23 -0700

    removed weird blanks

--- homework3/WordCount.java
+++ homework3/WordCount.java
@@ -13,10 +13,8 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class WordCount {
 	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
-
 		private final static IntWritable one = new IntWritable(1);
 		private Text word = new Text();
-
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 			StringTokenizer itr = new StringTokenizer(value.toString());
 			while (itr.hasMoreTokens()) {
