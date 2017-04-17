c0573cef5e85cbe29a30d0a8cf992f51c5dc2979
Shurui Liu <shurui91@gmail.com>
2017-04-16 20:33:32 -0700

    pagerank py file added

---
+++ homework4/pagerank.py
@@ -0,0 +1,33 @@
+__author__ = 'Shurui Liu'
+import networkx as nx
+
+def generatePageRank():
+	hashmap = readFile();
+	G = nx.DiGraph();
+	for key in hashmap:
+		G.add_node(key);
+		for url in hashmap[key]:
+			G.add_edge(key, url);
+
+	pr = nx.pagerank(G, alpha=0.85);
+	output = open("pagerank.txt", "w+");
+	for key in pr:
+		output.write("" + key + "=" + ("%.6f" % pr[key]) + "\n");
+	output.close();
+
+def readFile():
+	hashmap = {};
+	filename = "edgelist.txt";
+	with open(filename, "r") as lines:
+		for line in lines:
+			urls = line.split(' ');
+
+			if urls[0] not in hashmap:
+				hashmap[urls[0]] = [];
+
+			for url in urls[1:]:
+				hashmap[urls[0]].append(url);
+	return hashmap;
+
+if __name__ == "__main__":
+	generatePageRank();
